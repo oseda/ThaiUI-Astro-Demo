5c4d72d6ac0e580452d339c9aa96d31970985736
Menn <m@seedwebs.com>
2023-11-08 11:19:27 +0700

    Add Vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,6 @@
 import { defineConfig, passthroughImageService } from 'astro/config';
 import prefetch from '@astrojs/prefetch';
+import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
@@ -11,4 +12,6 @@ export default defineConfig({
       intentSelector: ["a[href^='/']"],
     }),
   ],
+  output: 'server',
+  adapter: vercel(),
 });
